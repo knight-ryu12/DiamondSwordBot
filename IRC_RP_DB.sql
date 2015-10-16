4972428897b1fe3340e3263fd2e75c63a3d4d50a
knight-ryu12 <knight.ryu12@gmail.com>
2015-10-16 18:55:24 +0900

    Erased Issue with COmmand

---
+++ IRC_RP_DB.sql
@@ -0,0 +1,4 @@
+INSERT INTO Chroma.IRC_RP_DB (id, Name, HP, Status, Weakness, Absoption, Resistence, Level, IRChost) VALUES (1, 'Chroma', 500, 'None', 'Light/Lightning', 'None', 'Mecha', 3, 'icysword.ml');
+INSERT INTO Chroma.IRC_RP_DB (id, Name, HP, Status, Weakness, Absoption, Resistence, Level, IRChost) VALUES (2, 'CidYoshi', 500, 'None', 'Dark/Fighting', 'None', 'Light/Mecha', 3, '148.225.143.147');
+INSERT INTO Chroma.IRC_RP_DB (id, Name, HP, Status, Weakness, Absoption, Resistence, Level, IRChost) VALUES (3, 'Annabelle', 800, 'None', 'Water/Earth', 'Fire', 'Darkness/Fight', 7, '1.23.247.112');
+INSERT INTO Chroma.IRC_RP_DB (id, Name, HP, Status, Weakness, Absoption, Resistence, Level, IRChost) VALUES (4, 'Neon', 800, 'Bored', 'Darkness/Water', 'Light', 'None', 7, '113.193.168.54');
---
+++ irc_person.sql
@@ -0,0 +1,2 @@
+INSERT INTO Chroma.irc_person (id, hostname, Msg) VALUES (1, 'icysword.ml', 'Hey Owner!');
+INSERT INTO Chroma.irc_person (id, hostname, Msg) VALUES (2, 'icysword.ml', 'TEST');
