a920f6574982856879e07ec009619eb1839ad9e7
Chroma ryu <knight.ryu12@gmail.com>
2015-12-27 22:12:12 +0900

    Sed

--- src/main/java/ds_bot/Sed.java
+++ src/main/java/ds_bot/Sed.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
  * Created by chroma on 15/12/25.
  */
 public class Sed {
-    @Override
+
     public void onMessage(MessageEvent event) {
         if(event.getMessage().startsWith("s/")) {
             ArrayList<Channel> chanlist = new ArrayList<Channel>();
-            for(Channel chan : event.getChannel()) chanlist.add(chan);
+            for(Channel chan : event.getBot().getUserChannelDao().getAllChannels()) chanlist.add(chan);
+
+
         }
     }
 }
