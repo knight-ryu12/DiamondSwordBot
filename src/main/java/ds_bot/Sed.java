cae2d25c6db74647bf0d193525c770167887c8ed
Chroma ryu <knight.ryu12@gmail.com>
2015-12-29 13:20:31 +0900

    Sed attempt #6

--- src/main/java/ds_bot/Sed.java
+++ src/main/java/ds_bot/Sed.java
@@ -5,6 +5,9 @@ import org.pircbotx.User;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -16,16 +19,31 @@ public class Sed extends ListenerAdapter{
     ArrayList<String> oldmessage = new ArrayList<String>();
     String oldmessagebuffer;
     String oldmessagechannel;
-    public void onMessage(MessageEvent event) {
-        int test;
-        if(event.getMessage().startsWith("?test4")) {
-            test = 0;
+    int test;
+    int channel;
+    String satisfied;
+    public void onMessage(MessageEvent event) throws IOException {
+        test = 0;
+        if(event.getMessage().startsWith("s/")) {
+            while(!event.getChannel().equals(chanlist.get(test))){
+                channel++;
+            }
+            // chanlist.get(test).getName() for getting channel whatever IRC ppl do this command.
+            //event.respond("You are talking at" + " " + chanlist.get(test).getName() +".");
+            String[] commandString = {"/bin/bash","-c","echo",oldmessage.get(channel).toString()," | "," sed "," -e "," '",event.getMessage(),"'"};
+            Process process = Runtime.getRuntime().exec(commandString);
+            BufferedReader in = new BufferedReader(new InputStreamReader(process
+                    .getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) event.getChannel().send().message(line);
+            in.close();
+        }
+        for(Channel chan : event.getBot().getUserChannelDao().getAllChannels().asList()) chanlist.add(chan);
+        // passive get old message. and old user too.
         while(!event.getChannel().equals(chanlist.get(test))){
             test++;
         }
-            event.respond("You are talking at" + " " + chanlist.get(test).getName() +".");
-        }
-        for(Channel chan : event.getBot().getUserChannelDao().getAllChannels().asList()) chanlist.add(chan);
-        for(User user : event.getChannel().getUsers().asList()) userlist.add(user);
+        if(!satisfied.contains("s/")) oldmessage.set(test, event.getMessage());
+        //for(User user : event.getChannel().getUsers().asList()) userlist.add(user);
     }
 }
